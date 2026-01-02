11ccabf9665ed29ccdc6681ba450292f270d5d82
k-vishal27 <vishalk.in27@gmail.com>
2026-01-02 20:55:37 +0530

    Add files via upload

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -1,1 +1,15 @@
+from typing import List
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        left = 0
+        while left < len(nums) - 1:
+            for i in range(left + 1, len(nums)):
+                if nums[left] + nums[i] == target:
+                    return [left, i]
+            left += 1
+
+
+s = Solution()
+print(s.twoSum([1, 2, 3, 4, 5], 8))
